a49fc0a39fc5a3b68447d4884ea541ce26dedf82
Sam <sam@wanfamily.org>
2023-09-13 20:04:03 -0700

    made the canon, and it rotates toward the cursor

--- src/main/Canon.java
+++ src/main/Canon.java
@@ -1,5 +1,57 @@
 package main;
-
+import acm.graphics.*;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 public class Canon {
+	private GOval canonBase;
+	private GRect canonBarrel;
+	private GCompound canonCompound = new GCompound();
+	private float angle = 90;
+	public Canon() {
+		canonBase = new GOval(100,100);
+		canonBarrel = new GRect(1, 50);
+		canonCompound.add(canonBase);
+		canonCompound.add(canonBarrel);
+		
+	}
+	public GOval getCanonBase() {
+		return canonBase;
+	}
+	public void setCanonBase(GOval canonBase) {
+		this.canonBase = canonBase;
+	}
+	public GRect getCanonBarrel() {
+		return canonBarrel;
+	}
+	public void setCanonBarrel(GRect canonBarrel) {
+		this.canonBarrel = canonBarrel;
+	}
+	public GCompound getCanonCompound() {
+		return canonCompound;
+	}
+	public void setCanonCompound(GCompound canonCompound) {
+		this.canonCompound = canonCompound;
+	}
+	public void pointToCurser(int mouseX, int mouseY) {
+		float xDistance = (float) (mouseX - canonBarrel.getX());
+		float yDistance = (float) (mouseY - canonBarrel.getY());
+		double rotationAngle = Math.toDegrees(Math.atan2(yDistance, xDistance));
+		if(angle < rotationAngle) {
+			canonBarrel.rotate(-1);
+			angle++;
+		}
+		if(angle > rotationAngle) {
+			canonBarrel.rotate(1);
+			angle--;
+		}
+
+		
+	}
+	public void centerCanon(int w, int h) {
+		canonBase.setLocation((w/2)-50, h-50);
+		canonBarrel.setLocation(((w/2)), h-50);
+	}
+	
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -9,8 +9,21 @@ public class Main extends GraphicsProgram{
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 1000;
 	public static final int INIT_X_VELOCITY = 5;
+	public Canon canon;
 	public void run() {
+		canon = new Canon();
+		canon.centerCanon(WINDOW_WIDTH, WINDOW_HEIGHT);
 		addMouseListeners();
+		add(canon.getCanonCompound());
+
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if(e != null) {
+			canon.pointToCurser(e.getX(), e.getY());
+		}
+
 	}
 	public void init() {
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -18,7 +31,6 @@ public class Main extends GraphicsProgram{
 	}
 	public static void main(String[] args) {
 
-		
 		new Main().start();
 		
 
